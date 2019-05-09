43ba1187c76a65b578f3de3b1f0e26180da6b18a
Rodrigo <rodrigowfranco@gmail.com>
2019-05-09 00:07:24 -0300

    Só limpando o código da API

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,11 +3,9 @@ class Requests {
     static API = 'https://dadosabertos.camara.leg.br/api/v2/';
 
     static get(apiArea, params) {
-        let uri = this.API + apiArea +
+        let uri = this.API + apiArea + '?' +
             Object.entries(params)
-                .map(([value, key], index) =>
-                    ((index===0 ? '?' : '&')+value+'='+key))
-                .join('');
+                .map(([value, key]) => (value+'='+key)).join('&');
         console.log(uri);
         return fetch(uri).then(response => response.json());
     }
